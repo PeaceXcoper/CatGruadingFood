3ac582d3e3d96a2d0f6b6f8b2bd04f0db8aaa2fa
PeaceXcoper <154996120+PeaceXcoper@users.noreply.github.com>
2024-11-09 02:05:57 +0700

    Catguradingfood.java

--- Catgame2/src/Catguradingfood.java
+++ Catgame2/src/Catguradingfood.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Catguradingfood extends JFrame {
